1347e13768470c5347942c9c2260a39b94b7a28a
kira <67888720+koffydrop@users.noreply.github.com>
2025-05-13 22:26:04 -0300

    add timer and delta time

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *  # noqa: F403
 
 def main():
     _, fail_count = pg.init()
+    clock = pg.time.Clock()
+    dt = 0
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # noqa: F405
     while True:
         for ev in pg.event.get():
@@ -12,6 +14,7 @@ def main():
                 return
         screen.fill(pg.color.Color(0, 0, 0))
         pg.display.flip()
+        dt = clock.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")  # noqa: F405
     print(f"Screen height: {SCREEN_HEIGHT}")  # noqa: F405
